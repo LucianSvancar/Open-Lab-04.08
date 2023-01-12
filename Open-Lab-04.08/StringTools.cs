c278181996573a726d14d911c4ae91353e7a3a4d
Lucián Švancár <svancarlucian@gmail.com>
2023-01-12 08:47:55 +0100

    Make funcion that takes an array of strings and returns the strings from the previous array which are 4 letters long.

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +7,26 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            int loopsNumber = 0;
+            int x = 0;
+            for (int a = 0; a < strings.Length; a++)
+            {
+                if (strings[a].Length == 4)
+                {
+                 loopsNumber++;
+                }
+            }
+            string[] newArray = new string[loopsNumber];
+
+            for (int b = 0; b < strings.Length; b++)
+            {
+                if (strings[b].Length == 4)
+                {
+                    newArray[x] = strings[b];
+                    x++;
+                }
+            }
+            return newArray;
         }
     }
 }
